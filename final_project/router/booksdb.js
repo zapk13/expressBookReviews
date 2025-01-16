14b39a0e9ced780af6d815711ba1ecd21eb144a9
Zohaib Ahmed <127920940+zapk13@users.noreply.github.com>
2025-01-17 03:17:06 +0500

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,87 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+  9780441013593: { author: "Frank Herbert", title: "Dune", reviews: {} },
+  9780684833392: { author: "Joseph Heller", title: "Catch-22", reviews: {} },
+  9780679417392: { author: "George Orwell", title: "1984", reviews: {} },
+  9780345363008: {
+    author: "Terry Brooks",
+    title: "The Talismans of Shannara",
+    reviews: {},
+  },
+  9780440418320: {
+    author: "Philip Pullman",
+    title: "The Golden Compass",
+    reviews: {},
+  },
+  9780679879251: {
+    author: "Philip Pullman",
+    title: "The Subtle Knife",
+    reviews: {},
+  },
+  9780194237628: {
+    author: "Jane Austen",
+    title: "Pride and Prejudice",
+    reviews: {},
+  },
+  9781451635621: {
+    author: "Margaret Mitchell",
+    title: "Gone With the Wind",
+    reviews: {},
+  },
+  9780142005149: {
+    author: "Charlotte Brontë",
+    title: "Jane Eyre",
+    reviews: {},
+  },
+  9780194237611: {
+    author: "Emily Brontë",
+    title: "Wuthering Heights",
+    reviews: {},
+  },
+  9780895773227: {
+    author: "Herman Melville",
+    title: "Moby Dick",
+    reviews: {},
+  },
+  9780141439563: {
+    author: "Charles Dickens",
+    title: "Great Expectations",
+    reviews: {},
+  },
+  9780679720201: {
+    author: "Albert Camus",
+    title: "The Stranger",
+    reviews: {},
+  },
+  9780140439441: {
+    author: "Charles Dickens",
+    title: "David Copperfield",
+    reviews: {},
+  },
+  9780140049978: {
+    author: "John Steinbeck",
+    title: "East of Eden",
+    reviews: {},
+  },
+  9781593080051: {
+    author: "Mary Shelley",
+    title: "Frankenstein",
+    reviews: {},
+  },
+  9780385333481: {
+    author: "Kurt Vonnegut",
+    title: "Cat's Cradle",
+    reviews: {},
+  },
+  9780517542095: {
+    author: "Douglas Adams",
+    title: "The Hitchhiker's Guide to the Galaxy",
+    reviews: {},
+  },
+  9780451191144: {
+    author: "Ayn Rand",
+    title: "Atlas Shrugged",
+    reviews: {},
+  },
+};
 
-module.exports=books;
+module.exports = books;
